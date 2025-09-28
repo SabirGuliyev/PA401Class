6f4190e47aeafec91d7542a59cf779dc61294ced
SabirGuliyev <sabir.guliyev@code.edu.az>
2025-09-28 14:52:07 +0400

    Datastructures added

---
+++ DataStructureExample/DataStructureExample/Program.cs
@@ -0,0 +1,220 @@
+﻿namespace DataStructureExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //int num = 100;
+
+            //int step = 0;
+            //for (int i = 0; i < num; i++)
+            //{
+            //   for (int j = 0; j < num; j++)
+            //    {
+            //        step++;
+            //        Console.WriteLine("Salam");
+            //    }
+            //}
+
+            //Console.WriteLine(step);
+
+
+            //if (num > 20)
+            //{
+            //    Console.WriteLine("Boyukdur");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("deyil");
+            //}
+
+
+
+
+
+
+
+
+
+
+
+
+            //Arr
+            //get Big O (1)
+            //Add and Remove Big O (n)
+
+
+            //LinkedList
+            //get Big O(n)
+            //Remove and Add  Big O(1)
+
+
+            //Stack
+            //Get Big O(1)
+            //Add/Remove O(1)
+
+            //Queue
+            //Get Big O(1)
+            //Add/Remove O(1)
+
+
+
+
+            //1          2          3        4       5
+            //string[] arr = { "Ferhad", "Siyavush", "Murad", "Jale", "Xanim","Test" };
+            ////////////////////  0          1          2        3       4        
+
+
+
+
+            //Console.WriteLine(arr[0]);
+
+
+
+
+            //int step = 0;
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    step++;
+            //    arr[i] += " PA401";
+            //    Console.WriteLine(arr[i]);
+            //}
+            //Console.WriteLine(step);
+
+
+
+
+            //int[] nums = { 23, 11, 4, 51, 12, 8 };
+            //int sum = 0;
+
+
+            //for(int i = 0; i < nums.Length; i++)
+            //{
+            //    if (nums[i] % 2 == 0)
+            //    {
+            //        sum += nums[i];
+            //        //Console.WriteLine(nums[i]);
+            //    }
+            //}
+
+            //Console.WriteLine(sum);
+
+
+
+
+
+
+
+
+            //int[,] nums =
+            //{
+            //   /*0*/ {1,2,3 },
+            //  /* 1*/ {4,5,6 },
+            //   /*2*/ {6,7,8}
+            //         //0,1,2
+            //};
+
+            //Console.WriteLine(nums.Rank);
+
+
+
+
+
+
+
+            //string name = "FerhAd";
+
+            //for (int i = 0;i<name.Length;i++)
+            //{
+            //    if (name[i] == 'a'|| name[i]=='A')
+            //    {
+            //        Console.WriteLine("Var");
+            //    }
+            //}
+
+
+
+
+
+
+            //arr[2]="Cingiz";
+
+            //Console.WriteLine(arr[arr.Length - 1]);
+
+
+
+
+
+
+
+            //int[] numbers = { 7, 22, 11, 90, 18 };
+            //int count = 0;
+
+            //for(int i = 0; i < numbers.Length; i++)
+            //{
+            //    for(int j = 2; j < numbers[i]; j++)
+            //    {
+            //        if (numbers[i] % j == 0)
+            //        {
+            //            count++;
+            //            break;
+            //        }
+            //    }
+            //}
+
+            //Console.WriteLine(count);
+
+
+
+
+            //LIFO
+            //Stack<string> values = new Stack<string>();
+
+            //values.Push("Ferhad");
+            //values.Push("Murad");
+
+
+            //Console.WriteLine(values.Pop());
+            //Console.WriteLine(values.Pop());
+
+
+            //FIFO
+            //Queue<string> queques=new Queue<string>();
+
+            //queques.Enqueue("Ferhad");
+            //queques.Enqueue("Murad");
+
+            //Console.WriteLine(queques.Dequeue());
+            //Console.WriteLine(queques.Dequeue());
+
+
+
+
+
+
+
+            //int number = 11;
+            //bool result= false;
+
+            //int step = 0;
+
+            //for (int i = 2; i*i<=number; i++)
+            //{
+            //    step++;
+            //    if (number % i == 0)
+            //    {
+            //        result = true;
+            //        break;
+            //    }
+            //}
+            //Console.WriteLine(result);
+            //Console.WriteLine(step);
+
+
+
+
+        }
+    }
+}
---
+++ HomeTask/HomeTask/Program.cs
@@ -0,0 +1,103 @@
+﻿
+#region Task 1
+//task 1
+//int range = 100;
+//int sum = 0;
+
+//int i = 1;
+//while (i < range)
+//{
+//    if (i % 2 == 0)
+//    {
+//        sum += i;
+//    }
+//    i++;
+//}
+//Console.WriteLine(sum);
+
+//for (int i = 1; i < 100; i++)
+//{
+//    if (i % 2 == 0)
+//    {
+//        sum += i;
+//    }
+//}
+
+//Console.WriteLine(sum); 
+#endregion
+
+#region Task 2
+//Task 2
+
+//int num = 6;
+//bool result = false;
+
+//if (num > 1)
+//{
+//    for (int i = 2; i < num; i++)
+//    {
+//        if (num % i == 0)
+//        {
+//            result = true;
+//            break;
+//        }
+//    }
+
+//    if (result)
+//    {
+//        Console.WriteLine("eded murekkebdir");
+//    }
+//    else
+//    {
+//        Console.WriteLine("Eded sadedir");
+//    }
+//}
+//else
+//{
+//    Console.WriteLine("Eded ne sade ne de murekkebdir");
+//} 
+#endregion
+
+#region Task 3
+
+//Task 3
+
+
+//int num = 123;
+
+
+//while (num!=0)
+//{
+//    Console.WriteLine(num%10);
+//    num /= 10;
+
+//} 
+#endregion
+
+#region Task 4
+
+
+//8 4 2   = 1  
+//16 8 4 2 = 1
+
+//12 6 = 3
+//14 = 7
+// 20 10 = 5
+
+
+//int num = 1;
+
+//while (num % 2 == 0)
+//{
+//    num /= 2;
+//}
+
+//if (num == 1)
+//{
+//    Console.WriteLine("2 quvvetidir");
+//}
+//else
+//{
+//    Console.WriteLine("Quvveti deyil");
+//} 
+#endregion
